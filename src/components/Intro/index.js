648627c52697922d39ce2db2c7a692bcd9e793bb
Harry <olivedreamdev@gmail.com>
2021-11-04 19:47:41 +0900

    complete Intro UI

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -10,20 +10,14 @@ export default function Intro() {
                 <img src={Intro_image} alt="intro_image" />
                 <p>NINE COLLECTIONS OF NINEHUNDRED NINETY NINE</p>
                 <img src={Retangular} alt="9x999" />
-                <h2>9 COLLECTIONS OF</h2><br/>
+                <h2 className="collections">9 COLLECTIONS OF</h2><br/>
                 <h1>999 UNIQUE M9Ξ</h1><br/>
                 <h2>// THE NUMBER NINE </h2><br/>
-                <p>The number nine is a very iconic element in the<br/> 
-                Daytoner universe.Nine is an amazing and very <br/>
-                unique number culturally but also mathematically. <br/>
-                In some way nine is the highest of all numbers,<br/>
-                because everything after that is just<br/>
-                combinations of lower numbers. In that sense it<br/>
-                symbolises a final frontier. The digital space<br/>
-                gives us the chance to explore M9E designs that<br/>
-                are not physically manufacturable and opens up<br/>
-                possibilities to sell digital and maybe even<br/>
-                physical one offsinspired by those new M9E creations.</p>
+                <div className="texts">
+                    <p>The number nine is a very iconic element in the Daytoner universe.Nine is an amazing and very unique number culturally but also mathematically.
+                    In some way nine is the highest of all numbers, because everything after that is just combinations of lower numbers. 
+                    In that sense it gives us the chance to explore M9E designs that are not physically manufacturable and opens up possibilities to sell digital and maybe even physical one offsinspired by those new M9E creations.</p>
+                </div>
             </div>
         </div>
     )
