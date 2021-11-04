6fe14d71f15ecc240b3d09b04ecfad25bddb34f6
Harry <olivedreamdev@gmail.com>
2021-11-04 10:08:55 +0900

    placed image and sentence

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import Intro from './components/Intro'
+import Intro from './components/Intro';
 import Footer from './components/Footer';
 import './App.css';
 
